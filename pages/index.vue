9a6da268bfaa17f7d6f0ab4cdb416456e0f224b2
InitalDevil <suryadavid@ymail.com>
2021-04-21 12:03:41 +0700

    fix spacing

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,58 @@
 <template>
-  <div>
-    <Carousel />
-    <div class="container p-5">
-      <h1 class="text-center font-weight-bold">
-        Program JongKreatif
-      </h1>
-      <p class="mt-2 text-center">
-        Cupcake danish tiramisu powder soda ginger cake. Dessert carrot cake tartsweet marzipan for fruitcake.
-      </p>
-      <div class="p-3 mt-4 text-center d-flex">
-        <div>
-          <h2 class="font-weight-bold text-danger">
-            JongKoding
-          </h2>
-          <p class="mt-3">
-            Pelatihan pemrograman bersertifikat, langsung dari developer dan inovator berpengalaman
-          </p>
-          <span class="font-weight-bold">
-            Pelajari Lebih
-          </span>
-        </div>
-        <div>
-          <h2 class="font-weight-bold text-warning">
-            JongDesain
-          </h2>
-          <p class="mt-3">
-            Pelatihan desain grafis, video, dan UIUX bersertifikat, langsung dari desainer dan inovator berpengalaman
-          </p>
-          <span class="font-weight-bold">
-            Pelajari Lebih
-          </span>
-        </div>
-        <div>
-          <h2 class="font-weight-bold text-info">
-            JongKonten
-          </h2>
-          <p class="mt-3">
-            Pelatihan pembuatan konten bersertifikat, langsung dari content creator dan inovator berpengalaman
-          </p>
-          <span class="font-weight-bold">
-            Pelajari Lebih
-          </span>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div>
+		<Carousel />
+		<div class="container p-5">
+			<h1 class="text-center font-weight-bold">
+				Program JongKreatif
+			</h1>
+			<p class="mt-2 text-center">
+				Cupcake danish tiramisu powder soda ginger cake. Dessert carrot cake tartsweet marzipan for fruitcake.
+			</p>
+			<div class="p-3 mt-4 text-center d-flex">
+				<div>
+					<h2 class="font-weight-bold text-danger">
+						JongKoding
+					</h2>
+					<p class="mt-3">
+						Pelatihan pemrograman bersertifikat, langsung dari developer dan inovator berpengalaman
+					</p>
+					<span class="font-weight-bold">
+						Pelajari Lebih
+					</span>
+				</div>
+				<div>
+					<h2 class="font-weight-bold text-warning">
+						JongDesain
+					</h2>
+					<p class="mt-3">
+						Pelatihan desain grafis, video, dan UIUX bersertifikat, langsung dari desainer dan inovator berpengalaman
+					</p>
+					<span class="font-weight-bold">
+						Pelajari Lebih
+					</span>
+				</div>
+				<div>
+					<h2 class="font-weight-bold text-info">
+						JongKonten
+					</h2>
+					<p class="mt-3">
+						Pelatihan pembuatan konten bersertifikat, langsung dari content creator dan inovator berpengalaman
+					</p>
+					<span class="font-weight-bold">
+						Pelajari Lebih
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import Carousel from '../components/Carousel'
 
 export default {
-  components: {
-    Carousel
-  }
+	components: {
+		Carousel
+	}
 }
 </script>
